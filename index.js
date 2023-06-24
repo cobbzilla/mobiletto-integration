1bfa43a572585c3624cdefc11f056313091ee6de
Jonathan Cobb <jonathan@kyuss.org>
2023-06-24 16:33:27 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { registerDriver } = require('mobiletto-base')
+
+const registerDrivers = () => {
+    registerDriver('local', require('mobiletto-driver-local'))
+    registerDriver('s3', require('mobiletto-driver-s3'))
+    registerDriver('b2', require('mobiletto-driver-b2'))
+}
+
+module.exports = { registerDrivers }
